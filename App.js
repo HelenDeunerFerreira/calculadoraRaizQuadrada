6ba75eb1f6ee1d1dc9961c98d093c64fd0e66336
HelenDeunerFerreira <helendeunerferreira@gmail.com>
2022-04-01 17:54:32 -0300

    Calculadora pronta

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [number, setNumber] = useState(" ");
+  const [resultado, setResultado] = useState(" ");
+
+  const calculaRaiz = () => {
+    let numero = parseInt(number)
+
+    if (numero != String) {
+      setResultado(Math.sqrt(numero))
+    } else {
+      setResultado("Número inválido")
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Digite o número para obter sua raiz quadrada</Text>
+
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        placeholder={" "}
+        onChangeText={(number) => setNumber(number)}
+        defaultValue={number}
+      />
+
+      <Button
+        onPress={() => calculaRaiz()}
+        title="Calcular"
+        color="#841030"
+      />
+
+      <Text style={styles.resultado}>Resultado: {resultado}</Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +44,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  input: {
+    height: 50,
+    width: 300,
+    alignItems: "center",
+    justifyContent: "center",
+    borderColor: "#000",
+    borderRadius: 15,
+    borderWidth: 2,
+    margin: 15,
+    padding: 10,
+  },
+  resultado: {
+    fontSize: 20,
+    margin: 15
+  }
 });
